bdb796d580c5c90fcd3280663d886c4dcf1c874e
julinsergey <mathservet@home.com>
2015-08-03 22:36:24 +0300

    .

--- test.m
+++ test.m
@@ -5,8 +5,8 @@ fprintf('..........START TEST\n');
 useHMM = 1;
 useHCRF = 0;
 useNPMPGM = 0;
-UCIDATASET = 1;
-TRAINFOLDSIZE = 10;
+UCIDATASET = 2;
+TRAINFOLDSIZE = 20;
 dataTrainUCI = getTrainData(1,UCIDATASET);
 dataTest = getTestData(1,UCIDATASET);
 %%
@@ -16,12 +16,13 @@ while (endD + TRAINFOLDSIZE) <= size(dataTrainUCI,2)
     endD = endD + TRAINFOLDSIZE;
     dataTrain = dataTrainUCI(:,1:endD);  
     index = index + 1;
+    RESULTMATRIX_X(1,index) = size(dataTrain,2);
     if (useHMM == 1)
-        nstates = 5;
-        nmix = 3;
+        nstates = 18;
+        nmix = 5;
         [PrecisionT, RecallT, F_mT, errorT, PrecisionTR, RecallTR, F_mTR, errorTR] = hmm_main(dataTrain,dataTest,nstates,nmix);
-        RESULTMARIX_TRAIN(1,index) = errorTR;   
-        RESULTMARIX_TEST(1,index) = errorT;
+        RESULTMATRIX_TRAIN(1,index) = errorTR;   
+        RESULTMATRIX_TEST(1,index) = errorT;
         fprintf('T HMM Average Precision  = %f\n', PrecisionT);
         fprintf('T HMM Average Recall  = %f\n', RecallT);
         fprintf('T HMM F-measure  = %f\n', F_mT);
@@ -30,8 +31,8 @@ while (endD + TRAINFOLDSIZE) <= size(dataTrainUCI,2)
         fprintf('TR HMM Average Recall  = %f\n', RecallTR);
         fprintf('TR HMM F-measure  = %f\n', F_mTR);
         fprintf('TR HMM Error  = %f\n', errorTR);
-        save('RESULTMARIX_TRAIN.mat', 'RESULTMARIX_TRAIN');
-        save('RESULTMARIX_TEST.mat', 'RESULTMARIX_TEST');
+        save('RESULTMATRIX_TRAIN.mat', 'RESULTMATRIX_TRAIN');
+        save('RESULTMATRIX_TEST.mat', 'RESULTMATRIX_TEST');
     end;
     if (useHCRF == 1)
         load sampleData;
@@ -50,8 +51,8 @@ while (endD + TRAINFOLDSIZE) <= size(dataTrainUCI,2)
         R{2}.params.regFactorL1 = 0;
         % R{2}.params.initWeights = initDataTransHMMtoHCRF;
         [PrecisionT, RecallT, F_mT, errorT, PrecisionTR, RecallTR, F_mTR, errorTR] = hcrf_main(dataTrain,dataTest,R);
-        RESULTMARIX_TRAIN(2,index) = errorTR;  
-        RESULTMARIX_TEST(2,index) = errorT;
+        RESULTMATRIX_TRAIN(2,index) = errorTR;  
+        RESULTMATRIX_TEST(2,index) = errorT;
         fprintf('T HCRF Average Precision  = %f\n', PrecisionT);
         fprintf('T HCRF Average Recall  = %f\n', RecallT);
         fprintf('T HCRF F-measure  = %f\n', F_mT);
@@ -60,8 +61,8 @@ while (endD + TRAINFOLDSIZE) <= size(dataTrainUCI,2)
         fprintf('TR HCRF Average Recall  = %f\n', RecallTR);
         fprintf('TR HCRF F-measure  = %f\n', F_mTR);
         fprintf('TR HCRF Error  = %f\n', errorTR);
-        save('RESULTMARIX_TRAIN.mat', 'RESULTMARIX_TRAIN');
-        save('RESULTMARIX_TEST.mat', 'RESULTMARIX_TEST');
+        save('RESULTMATRIX_TRAIN.mat', 'RESULTMATRIX_TRAIN');
+        save('RESULTMATRIX_TEST.mat', 'RESULTMATRIX_TEST');
      end;
      if (useNPMPGM == 1)
         %% Инициализация пула потоков (задание количества потоков)
@@ -77,8 +78,8 @@ while (endD + TRAINFOLDSIZE) <= size(dataTrainUCI,2)
         epohs_map = 100; % колличество эпох обучения карты Кохонена
         val_dirichlet = 0; % параметр распределения Дирихле
         [PrecisionT, RecallT, F_mT, errorT, PrecisionTR, RecallTR, F_mTR, errorTR] = npmpgm_main(dataTrain,dataTest,row_map,col_map,epohs_map,val_dirichlet);
-        RESULTMARIX_TRAIN(3,index) = errorTR;   
-        RESULTMARIX_TEST(3,index) = errorT;
+        RESULTMATRIX_TRAIN(3,index) = errorTR;   
+        RESULTMATRIX_TEST(3,index) = errorT;
         fprintf('T NPMPGM Average Precision  = %f\n', PrecisionT);
         fprintf('T NPMPGM Average Recall  = %f\n', RecallT);
         fprintf('T NPMPGM F-measure  = %f\n', F_mT);
@@ -87,9 +88,23 @@ while (endD + TRAINFOLDSIZE) <= size(dataTrainUCI,2)
         fprintf('TR NPMPGM Average Recall  = %f\n', RecallTR);
         fprintf('TR NPMPGM F-measure  = %f\n', F_mTR);
         fprintf('TR NPMPGM Error  = %f\n', errorTR);
-        save('RESULTMARIX_TRAIN.mat', 'RESULTMARIX_TRAIN');
-        save('RESULTMARIX_TEST.mat', 'RESULTMARIX_TEST');
+        save('RESULTMATRIX_TRAIN.mat', 'RESULTMATRIX_TRAIN');
+        save('RESULTMATRIX_TEST.mat', 'RESULTMATRIX_TEST');
      end;    
 end;
 %%
 fprintf('..........STOP TEST\n');
+
+%% PLOT
+set(0,'DefaultAxesFontSize',14,'DefaultAxesFontName','Times New Roman');
+set(0,'DefaultTextFontSize',14,'DefaultTextFontName','Times New Roman'); 
+figure('Units', 'normalized', 'OuterPosition', [0 0 1 1]);
+hold on;
+plot(RESULTMATRIX_X, RESULTMATRIX_TRAIN,'r','LineWidth',3);
+plot(RESULTMATRIX_X, RESULTMATRIX_TEST,'--','Color',[.1 .4 .1],'LineWidth',3);
+axis([-inf,inf,0,1])
+legend('ERROR TRAIN','ERROR TEST', 2);
+BX=get(gca,'XTick');
+BY=get(gca,'YTick');
+xlabel('Sample Size');
+ylabel('Error');
