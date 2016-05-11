980fa94d36ca40cb57d94599fc4842ca751c8d4b
Julin Serg From Job <julin.serg@gmail.com>
2016-05-11 17:05:10 +0400

    .

--- test.m
+++ test.m
@@ -10,15 +10,15 @@ end;
 fprintf('..........START TEST\n');
 %%
 use = 1; % HMM - 1  HCRF - 2 NPMPGM - 3
-UCIDATASET = 11;
-TRAINFOLDSIZE = 1;
+UCIDATASET = 1;
+TRAINFOLDSIZE = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 660];
 dataTrainUCI = getTrainData(1,UCIDATASET);
 dataTest = getTestData(1,UCIDATASET);
 %%
 endD = 0;
 index = 0;
-while (endD + TRAINFOLDSIZE) <= size(dataTrainUCI,2)    
-    endD = endD + TRAINFOLDSIZE;
+for ii = 1: size(TRAINFOLDSIZE,2) 
+    endD = TRAINFOLDSIZE(ii);
     dataTrain = dataTrainUCI(:,1:endD);  
     index = index + 1;
     RESULTMATRIX_X(use,index) = size(dataTrain,2);
@@ -27,7 +27,8 @@ while (endD + TRAINFOLDSIZE) <= size(dataTrainUCI,2)
     if (use == 1)
         nstates = 5;
         nmix = 0;
-        [PrecisionT, RecallT, F_mT, errorT, PrecisionTR, RecallTR, F_mTR, errorTR] = hmm_main(dataTrain,dataTest,nstates,nmix);
+        diag = 1;
+        [PrecisionT, RecallT, F_mT, errorT, PrecisionTR, RecallTR, F_mTR, errorTR] = hmm_main(dataTrain,dataTest,nstates,nmix,diag);
         RESULTMATRIX_TRAIN(use,index) = errorTR;   
         RESULTMATRIX_TEST(use,index) = errorT;
         fprintf('T HMM Average Precision  = %f\n', PrecisionT);
@@ -75,7 +76,7 @@ while (endD + TRAINFOLDSIZE) <= size(dataTrainUCI,2)
         %% Инициализация параметров классификатора    
         row_map = 1; % колличество строк карты Кохонена
         col_map = 5; % колличество столбцов карты Кохонена
-        epohs_map = 200; % колличество эпох обучения карты Кохонена
+        epohs_map = 500; % колличество эпох обучения карты Кохонена
         val_dirichlet = 0; % параметр распределения Дирихле
         [PrecisionT, RecallT, F_mT, errorT, PrecisionTR, RecallTR, F_mTR, errorTR] = npmpgm_main(dataTrain,dataTest,row_map,col_map,epohs_map,val_dirichlet);
         RESULTMATRIX_TRAIN(use,index) = errorTR;   
