87639dd6ae569e9bebd4f2ccdc6eb43d3adc8ea3
Julin Serg From Job <julin.serg@gmail.com>
2016-05-12 17:06:10 +0400

    .

--- test.m
+++ test.m
@@ -10,8 +10,9 @@ end;
 fprintf('..........START TEST\n');
 %%
 use = 3; % HMM - 1  HCRF - 2 NPMPGM - 3
-UCIDATASET = 1;
-TRAINFOLDSIZE = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 660];
+UCIDATASET = 2;
+%TRAINFOLDSIZE = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 660];
+TRAINFOLDSIZE = [6, 12, 18, 24, 30, 36, 42, 48];
 dataTrainUCI = getTrainData(1,UCIDATASET);
 dataTest = getTestData(1,UCIDATASET);
 %%
@@ -25,7 +26,7 @@ for ii = 1: size(TRAINFOLDSIZE,2)
     RESULTMATRIX_X(use,index) = size(dataTrain,2);
     RESULTMATRIX_X(use,index) = size(dataTrain,2);
     if (use == 1)
-        nstates = 5;
+        nstates = 7;
         nmix = 0;
         diag = 1;
         [PrecisionT, RecallT, F_mT, errorT, PrecisionTR, RecallTR, F_mTR, errorTR] = hmm_main(dataTrain,dataTest,nstates,nmix,diag);
@@ -75,7 +76,7 @@ for ii = 1: size(TRAINFOLDSIZE,2)
      if (use == 3)        
         %% Инициализация параметров классификатора    
         row_map = 1; % колличество строк карты Кохонена
-        col_map = 5; % колличество столбцов карты Кохонена
+        col_map = 7; % колличество столбцов карты Кохонена
         epohs_map = 500; % колличество эпох обучения карты Кохонена
         val_dirichlet = 0; % параметр распределения Дирихле
         [PrecisionT, RecallT, F_mT, errorT, PrecisionTR, RecallTR, F_mTR, errorTR] = npmpgm_main(dataTrain,dataTest,row_map,col_map,epohs_map,val_dirichlet);
