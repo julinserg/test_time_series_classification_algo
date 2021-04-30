f0d255c4503534fb9423c558bf778482b8e36320
ss.yulin <ss.yulin@gitlab.com>
2021-04-30 17:42:22 +0300

    [ 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 30, 35, 40, 45, 50]

--- MLSTM-FCN.py
+++ MLSTM-FCN.py
@@ -95,8 +95,7 @@ INDEX_DATASETS_LIST = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 for INDEX_DATASET in INDEX_DATASETS_LIST:
     DATA = ('ArticularyWordRecognition' + '-' + str(INDEX_DATASET) , 0 + (INDEX_DATASET-1)*25)
 
-    TRAINFOLDSIZE  = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
-        17, 18, 19, 20, 25, 30, 35, 40, 45, 50]
+    TRAINFOLDSIZE  = [ 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 30, 35, 40, 45, 50]
 
     dataIndex = 0
     RESULT_ACCURACY = []
--- plotResults.py
+++ plotResults.py
@@ -21,8 +21,7 @@ dictModel['MLSTM-FCN'] = ModelStruct('AccuracySeq(model - MLSTM-FCN).csv', 'cyan
 
 dataSetName = set()
 dictAccuracySeqByModel = {}
-axisX  = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
-    17, 18, 19, 20, 25, 30, 35, 40, 45, 50]
+axisX  = [ 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 30, 35, 40, 45, 50]
 
 
 
