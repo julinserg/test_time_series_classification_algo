2e83a607a50985114d0d58824dc0fff92f57f29b
julinsergey <mathservet@home.com>
2021-05-08 21:01:25 +0300

    TRAINFOLDSIZE  = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, ...     17, 18, 19, 20, 21, 22, 23, 24, 25];

--- MLSTM-FCN.py
+++ MLSTM-FCN.py
@@ -95,11 +95,12 @@ if not os.path.exists('weights'):
 if not os.path.exists('mlstm_fcn_csv'):
     os.makedirs('mlstm_fcn_csv')
 
-INDEX_DATASETS_LIST = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+INDEX_DATASETS_LIST = [1]
 for INDEX_DATASET in INDEX_DATASETS_LIST:
     DATA = ('ArticularyWordRecognition' + '-' + str(INDEX_DATASET) , 0 + (INDEX_DATASET-1)*25)
 
-    TRAINFOLDSIZE  = [ 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25]
+    TRAINFOLDSIZE  = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+                        17, 18, 19, 20, 21, 22, 23, 24, 25]
 
     dataIndex = 0
     RESULT_ACCURACY = []
